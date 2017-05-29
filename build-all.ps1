6e08eae8d9236927f7881135e6d49c7be8c451e7
Khalifah K. Shabazz <shabazzk@gmail.com>
2017-05-29 16:13:25 -0400

    Added comments on how to run the powershell script.

--- build-all.ps1
+++ build-all.ps1
@@ -4,3 +4,7 @@ docker build -t khalifahks/area51:mongodb ./centos7-mongodb
 docker build -t khalifahks/area51:nginx ./centos7-nginx
 docker build -t khalifahks/area51:php70 ./centos7-php
 docker build -t khalifahks/area51:web-app ./git-php-app
+<#
+To run this without enable Execution policy:
+powershell -ExecutionPolicy bypass -File .\build-all.ps1
+#>
