ec59f3ba73aa271a6119a7f904d01e0d33d01d64
Khalifah K. Shabazz <shabazzk@gmail.com>
2017-05-29 23:45:56 -0400

    Removed the build of the web-app image for now.

--- build-all.ps1
+++ build-all.ps1
@@ -3,7 +3,7 @@ docker build -t khalifahks/area51:memcached ./centos7-memcached
 docker build -t khalifahks/area51:mongodb ./centos7-mongodb
 docker build -t khalifahks/area51:nginx ./centos7-nginx
 docker build -t khalifahks/area51:php70 ./centos7-php
-docker build -t khalifahks/area51:web-app ./git-php-app
+#docker build -t khalifahks/area51:web-app ./git-php-app
 <#
 To run this without enable Execution policy:
 powershell -ExecutionPolicy bypass -File .\build-all.ps1
