fec6c4956ae49f279bc2d43437233a5e79c113b1
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2017-07-12 16:37:49 -0400

    Added some status output to the shell build script.

--- build-all.sh
+++ build-all.sh
@@ -8,10 +8,15 @@ docker rm centos-nginx centos-mongodb centos-memcached centos-php
 docker image prune -a
 
 # Build all the images. The order matters.
+printf "\n\nBuilding memcached\n";
 docker build -t khalifahks/centos-memcached ./centos7-memcached
+printf "\n\nBuilding mongodb\n";
 docker build -t khalifahks/centos-mongodb ./centos7-mongodb
+printf "\n\nBuilding nginx\n";
 docker build -t khalifahks/centos-nginx ./centos7-nginx
+printf "\n\nBuilding php\n";
 docker build -t khalifahks/centos-php ./centos7-php
+printf "\n\nBuilding apps\n";
 docker build -t khalifahks/centos-apps ./apps
 
 # NOTES: See build-all.ps1 for command and hints.
