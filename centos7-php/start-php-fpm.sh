d38c386f28abc9f050e28c09d107f1c2e54a4c43
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2018-03-21 19:23:47 -0400

    Changes to PHP 7 containers.
    * Added new script to start PHP-FPM and tail log files to feed docker.
    * Updated entry points.

---
+++ centos7-php/start-php-fpm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+php-fpm -D
+
+touch /var/log/php-fpm.log
+touch /var/log/php-fpm/www.log
+
+chown -R apache:apache /var/log/php-fpm.log
+chown -R apache:apache /var/log/php-fpm
+chown -R apache:apache /var/log/php-fpm/www.log
+
+tail -f /var/log/php-fpm.log \
+     -f /var/log/php-fpm/www.log
