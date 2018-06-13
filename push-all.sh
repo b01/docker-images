6e359c71e6efedfe9fb313133674ac863bcf3b06
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2018-06-13 14:36:18 -0400

    Added Circle CI image to push script.

--- push-all.sh
+++ push-all.sh
@@ -2,8 +2,9 @@
 
 docker push khalifahks/centos-nginx
 docker push khalifahks/centos-memcached
-docker push khalifahks/centos-mongodb
 docker push khalifahks/centos-php
 docker push khalifahks/centos-php:54
 docker push khalifahks/centos-apps
 docker push khalifahks/centos-apps:54
+docker push khalifahks/centos-cci
+docker push khalifahks/centos-mongodb
