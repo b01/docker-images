1f0ead0068822e635c02c094773932ccf61c5d53
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2018-06-13 13:23:09 -0400

    Added Chrome browser and a virtual monitor to the apps container.

--- build-all.sh
+++ build-all.sh
@@ -6,8 +6,6 @@ docker build --rm -t khalifahks/centos-nginx ./centos7-nginx
 
 docker build --rm -t khalifahks/centos-memcached ./centos7-memcached
 
-docker build --rm -t khalifahks/centos-mongodb ./centos7-mongodb
-
 docker build --rm -t khalifahks/centos-php ./centos7-php
 
 docker build --rm -t khalifahks/centos-php:54 ./centos7-php54
@@ -15,5 +13,7 @@ docker build --rm -t khalifahks/centos-php:54 ./centos7-php54
 docker build --rm -t khalifahks/centos-apps ./apps
 
 docker build --rm -t khalifahks/centos-apps:54 ./apps54
+
+docker build --rm -t khalifahks/centos-mongodb ./centos7-mongodb
 
 #docker build --rm -t khalifahks/centos-mysql ./centos7-mysql
