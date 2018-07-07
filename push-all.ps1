64c21f472ab62e7c385b8fd89eb563f2c8790042
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2018-07-07 05:22:42 -0400

    Updated push-all script for PowerShell.

--- push-all.ps1
+++ push-all.ps1
@@ -1,7 +1,5 @@
-docker push khalifahks/centos-nginx
-docker push khalifahks/centos-memcached
-docker push khalifahks/centos-mongodb
-docker push khalifahks/centos-php
-docker push khalifahks/centos-php:54
-docker push khalifahks/centos-apps
-docker push khalifahks/centos-apps:54
+docker push khalifahks/alpine-nginx
+docker push khalifahks/alpine-memcached
+docker push khalifahks/alpine-mongodb
+docker push khalifahks/alpine-php
+docker push khalifahks/alpine-apps
