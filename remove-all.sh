22af2f2ae26b8b31c65db20be8a3792a3ff7aa23
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2018-07-09 16:37:15 -0400

    Updated the remove-all shell command.

--- remove-all.sh
+++ remove-all.sh
@@ -3,13 +3,17 @@
 # This script removes all containers and associated images.
 
 # Remove containers.
+docker rm alpine-nginx
+docker rm alpine-memcached
 docker rm alpine-apps
 docker rm alpine-php
-docker rm alpine-nginx
 docker rm alpine-mongodb
+docker rm alpine-mongodb-support
 
 # Remove images.
-docker rmi khalifahks/alpine-apps
 docker rmi khalifahks/alpine-nginx
+docker rmi khalifahks/alpine-memcached
+docker rmi khalifahks/alpine-apps
 docker rmi khalifahks/alpine-php
 docker rmi khalifahks/alpine-mongodb
+docker rmi khalifahks/alpine-mongodb-support
