0969741aade2b4b3d354f303ac70dafd05b5c7d4
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2018-07-17 11:05:27 -0400

    Updated shell build-all command to not use cache.

--- build-all.sh
+++ build-all.sh
@@ -2,12 +2,16 @@
 
 # Build all the images. The order matters for app containers.
 
-docker build --rm -t khalifahks/alpine-nginx ./alpine-nginx
+docker build --rm --no-cache -t khalifahks/alpine-nginx ./alpine-nginx
 
-docker build --rm -t khalifahks/alpine-php ./alpine-php
+docker build --rm --no-cache -t khalifahks/alpine-memcached ./alpine-memcached
 
-docker build --rm -t khalifahks/alpine-apps ./alpine-apps
+docker build --rm --no-cache -t khalifahks/alpine-php ./alpine-php
 
-#docker build --rm -t khalifahks/alpine-cci cci
+docker build --rm --no-cache -t khalifahks/alpine-apps ./alpine-apps
 
-docker build --rm -t khalifahks/alpine-mongodb ./alpine-mongodb
+docker build --rm --no-cache -t khalifahks/alpine-cci cci
+
+docker build --rm --no-cache -t khalifahks/alpine-mongodb ./alpine-mongodb
+
+docker build --rm --no-cache -t khalifahks/alpine-mongodb-support ./alpine-mongodb-support
