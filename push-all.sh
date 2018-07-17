0cc7774843fb928b1aab2b4ba3666247e5b333c4
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2018-07-17 11:04:47 -0400

    Updated push-all commands to push alpine containers.

--- push-all.sh
+++ push-all.sh
@@ -4,4 +4,6 @@ docker push khalifahks/alpine-nginx
 docker push khalifahks/alpine-memcached
 docker push khalifahks/alpine-php
 docker push khalifahks/alpine-apps
+docker push khalifahks/alpine-cci
 docker push khalifahks/alpine-mongodb
+docker push khalifahks/alpine-mongodb-support
