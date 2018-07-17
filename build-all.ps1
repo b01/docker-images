464c1c54d9257d96afda471ba336cce58e477eaf
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2018-07-17 11:06:35 -0400

    Updated Powershell build-all command to not use alpine containers.

--- build-all.ps1
+++ build-all.ps1
@@ -2,19 +2,17 @@
 
 # Build all the images. The order matters for app containers.
 
-docker build --rm -t khalifahks/centos-nginx ./centos7-nginx
+docker build --rm --no-cache -t khalifahks/alpine-nginx ./alpine-nginx
 
-docker build --rm -t khalifahks/centos-memcached ./centos7-memcached
+docker build --rm --no-cache -t khalifahks/alpine-memcached ./alpine-memcached
 
-docker build --rm -t khalifahks/centos-mongodb ./centos7-mongodb
+docker build --rm --no-cache -t khalifahks/alpine-php ./alpine-php
 
-docker build --rm -t khalifahks/centos-php ./centos7-php
+docker build --rm --no-cache -t khalifahks/alpine-apps ./alpine-apps
 
-docker build --rm -t khalifahks/centos-php:54 ./centos7-php54
+docker build --rm --no-cache -t khalifahks/alpine-cci cci
 
-docker build --rm -t khalifahks/centos-apps ./apps
+docker build --rm --no-cache -t khalifahks/alpine-mongodb ./alpine-mongodb
 
-docker build --rm -t khalifahks/centos-apps:54 ./apps54
-
-#docker build --rm -t khalifahks/centos-mysql ./centos7-mysql
+docker build --rm --no-cache -t khalifahks/alpine-mongodb-support ./alpine-mongodb-support
 
