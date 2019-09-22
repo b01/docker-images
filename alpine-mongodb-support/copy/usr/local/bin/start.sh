f99557398bd87b27e18c27518c7dd85f5093b397
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2019-09-22 16:49:47 -0400

    Updated Alpine Mongo DB Support start script.

--- alpine-mongodb-support/copy/usr/local/bin/start.sh
+++ alpine-mongodb-support/copy/usr/local/bin/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Because crond does not exit gracefully, we'll use to manage starting and stopping this container.
+# Because crond does not exit gracefully, we'll use this script to manage starting and stopping this container.
 
 set -e
 
@@ -9,7 +9,7 @@ running=0
 
 shutd () {
     # echo "Shutting down MongoDB Support cron job..." >&2
-    printf "Shutting down MongoDB Support cron job..." >&2
+    printf "Shutting down MongoDB Support cron job process..."
     kill -9 `cat /var/run/crond.pid`
     keepRunning=false # set this to false to exit the loop we started earlier.
     printf "done.\n"
@@ -17,16 +17,15 @@ shutd () {
 
 trap 'shutd' SIGTERM
 
-echo "start.sh  Pid=$$"
+# echo "start.sh  Pid=$$"
 echo "$$" > /var/run/start.pid
 
-echo "starting MongDB support cron job, all interaction with the server will output below:"
+echo "Starting MongDB support cron job, all interaction with the server will output below."
 
-# Start the cron job to take Mongo DB backups in the foreground to keep the container running.
-# exec crond -f
+# Start the cron job in the background to take Mongo DB backups.
 crond
 
-# Keep this script running in the foreground as PID 1.
+# This loop will keep this script running in the foreground.
 while $keepRunning
 do
     running=1
