bed02f312d17fa0d1473550134ed0d54c9b3e60b
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-03-10 07:13:07 -0500

    Update CI Build Scripts
    These scripts auto build and publish some of the images in the CI
    workflow. This change update which images are automatically published.
    
    rmv: alpine-php and alpine-app images from CI auto-build.

--- remove-all.ps1
+++ remove-all.ps1
@@ -2,18 +2,14 @@
 
 # Remove containers.
 docker rm alpine-nginx
-docker rm alpine-memcached
 docker rm alpine-cci
-docker rm alpine-apps
-docker rm alpine-php
+docker rm alpine-glibc
 docker rm alpine-mongodb
 docker rm alpine-mongodb-support
 
 # Remove images.
 docker rmi khalifahks/alpine-nginx
-docker rmi khalifahks/alpine-memcached
 docker rmi khalifahks/alpine-cci
-docker rmi khalifahks/alpine-apps
-docker rmi khalifahks/alpine-php
+docker rmi khalifahks/alpine-glibc
 docker rmi khalifahks/alpine-mongodb
 docker rmi khalifahks/alpine-mongodb-support
